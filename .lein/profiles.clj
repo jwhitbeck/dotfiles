9c7ab058fb566b15132f1f5f922ecb19a42da1b0
Sean Whitbeck <sean@liftoff.io>
2014-10-20 09:12:45 -0700

    [lein] Add lein-cloverage

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,6 +5,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   [lein-ancient "0.5.5"]
                   [lein-clojars "0.9.1"]
+                  [lein-cloverage "1.0.2"]
                   [lein-exec "0.3.1"]
                   [lein-kibit "0.0.8"]
                   [lein-try "0.4.1"]
