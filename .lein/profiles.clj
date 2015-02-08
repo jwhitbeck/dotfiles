cb9aa5e632e0321cc4e7071fe381fbc979925c3f
John Whitbeck <john@whitbeck.fr>
2015-02-07 17:01:15 -0800

    [lein] Upgrade vinyasa

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -15,23 +15,25 @@
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.trace "0.7.6"]
                        [org.clojure/tools.namespace "0.2.4"]
-                       [im.chit/vinyasa "0.2.0"]
+                       [leiningen #=(leiningen.core.main/leiningen-version)]
+                       [im.chit/vinyasa "0.3.0"]
                        [spyscope "0.1.4"]
                        [slamhound "1.5.1"]
                        [io.aviso/pretty "0.1.8"]]
-        :injections [(require '[vinyasa.inject :as inj])
-                     (inj/inject 'clojure.core
-                                 '[[vinyasa.inject inject]
-                                   [vinyasa.pull pull]
-                                   [clojure.stacktrace e print-stack-trace]
-                                   [clojure.tools.trace trace-ns trace-vars untrace-ns untrace-vars]
-                                   [clojure.tools.namespace.repl refresh]
-                                   [criterium.core bench quick-bench]])
-                     (require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
-                     ;; Pretty printing of stackstraces
+        :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
+                     (require '[vinyasa.inject :as inj])
                      (require 'io.aviso.repl
                               'clojure.repl
                               'clojure.main)
+                     (inj/in clojure.core
+                             [vinyasa.inject :refer [inject]]
+                             [vinyasa.pull :all]
+                             [vinyasa.reimport :all]
+                             [clojure.stacktrace :refer [e print-stack-trace]]
+                             [clojure.tools.trace :refer [trace-ns trace-vars untrace-ns untrace-vars]]
+                             [clojure.tools.namespace.repl :refer [refresh]]
+                             [criterium.core :refer [bench quick-bench]])
+                     ;; Pretty printing of stackstraces
                      (alter-var-root #'clojure.repl/pst
                                      (constantly @#'io.aviso.repl/pretty-pst))]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
