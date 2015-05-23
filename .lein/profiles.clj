2038c7265b1c276715ac04ea0ae1b415f8af800a
John Whitbeck <john@whitbeck.fr>
2015-05-23 15:30:08 -0700

    [lein] Remove vinyasa

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -16,27 +16,14 @@
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.trace "0.7.6"]
                        [org.clojure/tools.namespace "0.2.4"]
-                       [leiningen #=(leiningen.core.main/leiningen-version)]
-                       [im.chit/vinyasa "0.3.0"]
                        [spyscope "0.1.4"]
                        [slamhound "1.5.1"]
                        [io.aviso/pretty "0.1.8"]]
         :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
-                     (require '[vinyasa.inject :as inj])
-                     (require 'io.aviso.repl
-                              'clojure.repl
-                              'clojure.main)
-                     (inj/in clojure.core
-                             [vinyasa.inject :refer [inject]]
-                             [vinyasa.pull :all]
-                             ; Disabled because it causes compilation errors in hyperloglog
-                             ;[vinyasa.reimport :all]
-                             [clojure.stacktrace :refer [e print-stack-trace]]
-                             [clojure.tools.trace :refer [trace-ns trace-vars untrace-ns untrace-vars]]
-                             [clojure.tools.namespace.repl :refer [refresh]]
-                             [criterium.core :refer [bench quick-bench]]
-                             [no.disassemble :refer [disassemble]])
+                     (require '[criterium.core :refer [bench quick-bench]])
+                     (require '[no.disassemble :refer [disassemble]])
                      ;; Pretty printing of stackstraces
+                     (require 'io.aviso.repl 'clojure.repl)
                      (alter-var-root #'clojure.repl/pst
                                      (constantly @#'io.aviso.repl/pretty-pst))]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
