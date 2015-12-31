29c871eaceee9f46a81fb1b118238a77f0d62436
John Whitbeck <john@whitbeck.fr>
2015-12-30 18:00:44 -0800

    [lein] Cleanup

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,28 +1,17 @@
-;;; Resources
-;;;  - https://github.com/zcaudate/vinyasa
-;;;  - http://z.caudate.me/give-your-clojure-workflow-more-flow/
-
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]
-                  [lein-ancient "0.5.5"]
-                  [lein-bikeshed "0.1.8"]
+                  [lein-ancient "0.6.8"]
                   [lein-clojars "0.9.1"]
-                  [lein-cloverage "1.0.2"]
-                  [lein-exec "0.3.1"]
                   [lein-kibit "0.1.2"]
                   [lein-nodisassemble "0.1.3"]
-                  [lein-try "0.4.1"]
-                  [jonase/eastwood "0.2.2"]]
+                  [lein-try "0.4.3"]
+                  [jonase/eastwood "0.2.3"]]
         :dependencies [[criterium "0.4.3"]
-                       [org.clojure/tools.trace "0.7.6"]
-                       [org.clojure/tools.namespace "0.2.4"]
-                       [spyscope "0.1.4"]
-                       [slamhound "1.5.1"]
-                       [io.aviso/pretty "0.1.8"]]
+                       [org.clojure/tools.trace "0.7.9"]
+                       [org.clojure/tools.namespace "0.2.11"]
+                       [spyscope "0.1.5"]
+                       [slamhound "1.5.5"]]
         :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
                      (require '[criterium.core :refer [bench quick-bench]])
                      (require '[no.disassemble :refer [disassemble]])
-                     ;; Pretty printing of stackstraces
-                     (require 'io.aviso.repl 'clojure.repl)
-                     (alter-var-root #'clojure.repl/pst
-                                     (constantly @#'io.aviso.repl/pretty-pst))]
+                     (require '[clojure.tools.namespace.repl :refer [refresh]])]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
