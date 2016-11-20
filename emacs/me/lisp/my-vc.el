b14a629ec498ec0e0afcf0196357b4b8c17f9786
John Whitbeck <john@whitbeck.net>
2016-11-20 09:48:08 -0800

    [emacs] Improve vc-git-grep

--- emacs/me/lisp/my-vc.el
+++ emacs/me/lisp/my-vc.el
@@ -7,8 +7,14 @@
 
 (custom-set-variables '(magit-delete-by-moving-to-trash nil))
 
-;;; Bind git-grep
+;;; Git grep
 (autoload 'vc-git-grep "vc-git")
+
+;;; Quote default pattern to prevent shell expansion of wildcards
+(defun my-quote-grep-read-files (pattern)
+  (concat "\"" pattern "\""))
+
+(advice-add 'grep-read-files :filter-return 'my-quote-grep-read-files)
 
 (global-set-key (kbd "C-x g") 'vc-git-grep)
 
