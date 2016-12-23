cf9023a4e41e52923b1b9bcb5023a805cc5fe88a
Sean Whitbeck <sean@liftoff.io>
2016-12-23 09:51:59 -0800

    [lein] Remove tools.namespace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -8,9 +8,7 @@
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[criterium "0.4.3"]
                        [org.clojure/tools.trace "0.7.9"]
-                       [org.clojure/tools.namespace "0.2.11"]
                        [spyscope "0.1.5"]]
         :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
                      (require '[criterium.core :refer [bench quick-bench]])
-                     (require '[no.disassemble :refer [disassemble]])
-                     (require '[clojure.tools.namespace.repl :refer [refresh]])]}}
+                     (require '[no.disassemble :refer [disassemble]])]}}
