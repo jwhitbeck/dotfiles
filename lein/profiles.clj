fc54d0585e02284dd7eef28d293d5663be3be669
Sean Whitbeck <sean@liftoff.io>
2016-12-25 06:17:39 -0800

    [lein] Remove spyscope

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,8 +4,6 @@
                   [lein-kibit "0.1.2"]
                   [lein-nodisassemble "0.1.3"]
                   [jonase/eastwood "0.2.3"]]
-        :dependencies [[criterium "0.4.3"]
-                       [spyscope "0.1.5"]]
-        :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
-                     (require '[criterium.core :refer [bench quick-bench]])
+        :dependencies [[criterium "0.4.3"]]
+        :injections [(require '[criterium.core :refer [bench quick-bench]])
                      (require '[no.disassemble :refer [disassemble]])]}}
