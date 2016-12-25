08aeb9eab4cb7a53402d878f5281a70c5a581568
Sean Whitbeck <sean@liftoff.io>
2016-12-25 03:26:52 -0800

    [lein] Remove tools.trace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -6,7 +6,6 @@
                   [lein-nodisassemble "0.1.3"]
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[criterium "0.4.3"]
-                       [org.clojure/tools.trace "0.7.9"]
                        [spyscope "0.1.5"]]
         :injections [(require 'spyscope.core) ; for the reader macro #spy/d, available in all namespaces
                      (require '[criterium.core :refer [bench quick-bench]])
