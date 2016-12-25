6930b645f386c2cfacaa2fa36299c7e1505be958
Sean Whitbeck <sean@liftoff.io>
2016-12-25 06:26:22 -0800

    [lein] Upgrade kibit

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,10 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0"]
-                  [lein-kibit "0.1.2"]
                   ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
                   ;; https://github.com/xsc/lein-ancient
                   [lein-ancient "0.6.10"]
+                  ;; kibit is a static code analyzer for Clojure. Use with kibit-helper emacs package.
+                  ;; https://github.com/jonase/kibit
+                  [lein-kibit "0.1.3"]
                   [lein-nodisassemble "0.1.3"]
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[criterium "0.4.3"]]
