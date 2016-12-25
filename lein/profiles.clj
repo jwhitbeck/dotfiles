af5cf68bfec3e8c17d180eef7e748c789a1fa1d2
Sean Whitbeck <sean@liftoff.io>
2016-12-25 06:20:48 -0800

    [lein] Upgrade lein-ancient

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0"]
-                  [lein-ancient "0.6.8"]
                   [lein-kibit "0.1.2"]
+                  ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
+                  ;; https://github.com/xsc/lein-ancient
+                  [lein-ancient "0.6.10"]
                   [lein-nodisassemble "0.1.3"]
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[criterium "0.4.3"]]
