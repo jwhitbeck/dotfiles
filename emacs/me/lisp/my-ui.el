efce4f90445eb7512b8f2c93d06d511b0740ab42
Sean Whitbeck <sean@liftoff.io>
2016-12-25 07:06:01 -0800

    [emacs] Minor refactor

--- emacs/me/lisp/my-ui.el
+++ emacs/me/lisp/my-ui.el
@@ -194,12 +194,12 @@
 ;; See https://github.com/company-mode/company-mode/issues/180
 (defvar-local company-fci-mode-on-p nil)
 
-(defun company-turn-off-fci (&rest ignore)
+(defun company-turn-off-fci (&rest _)
   (when (boundp 'fci-mode)
     (setq company-fci-mode-on-p fci-mode)
     (when fci-mode (fci-mode -1))))
 
-(defun company-maybe-turn-on-fci (&rest ignore)
+(defun company-maybe-turn-on-fci (&rest _)
   (when company-fci-mode-on-p (fci-mode 1)))
 
 (add-hook 'company-completion-started-hook 'company-turn-off-fci)
