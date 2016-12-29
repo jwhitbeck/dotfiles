06f46a66e0f819cde2fde6a70feb367ad9fe8b82
Sean Whitbeck <sean@liftoff.io>
2016-12-29 08:21:36 -0800

    [lein] Add some exclusions

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,14 +4,14 @@
                   [lein-ancient "0.6.10"]
                   ;; kibit is a static code analyzer for Clojure. Use with kibit-helper emacs package.
                   ;; https://github.com/jonase/kibit
-                  [lein-kibit "0.1.3"]
+                  [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
                   ;; A Clojure library designed to let you inspect bytecode of functions and things.
                   ;; https://github.com/gtrak/no.disassemble
                   [lein-nodisassemble "0.1.3"]
                   ;; Eastwood is a Clojure lint tool that uses the tools.analyzer and tools.analyzer.jvm
                   ;; libraries to inspect namespaces and report possible problems.
                   ;; https://github.com/jonase/eastwood
-                  [jonase/eastwood "0.2.3"]]
+                  [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]
         :dependencies [[criterium "0.4.3"]]
         ;; Make these functions available in all namespaces
         :injections [(require '[criterium.core :refer [bench quick-bench]])
