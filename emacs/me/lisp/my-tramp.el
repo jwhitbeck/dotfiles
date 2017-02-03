b9d0be657cebef2f2a86dcf11b82391099c3b519
John Whitbeck <john@whitbeck.net>
2017-02-02 18:28:19 -0800

    [emacs] Use default tramp cache settings

--- emacs/me/lisp/my-tramp.el
+++ emacs/me/lisp/my-tramp.el
@@ -10,10 +10,6 @@
  '(tramp-default-proxies-alist
    (let ((local-host-regex (concat "\\(127\\.0\\.0\\.1\\|::1\\|localhost6?\\|" system-name "\\)")))
      `((,local-host-regex "root" nil) (".*" "root" "/ssh:%h:"))))
- ;; Always use the remote file-name cache for read access
- '(remote-file-name-inhibit-cache nil)
- ;; Always use the cache for remote completion
- '(tramp-completion-reread-directory-timeout nil)
  ;; Disable version control checks on remote files
  '(vc-ignore-dir-regexp (format "\\(%s\\)\\|\\(%s\\)" vc-ignore-dir-regexp tramp-file-name-regexp))
  ;; Use ssh control master config from ~/.ssh/config
