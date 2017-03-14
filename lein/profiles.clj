ccebc20b8bd69844ab368a5569e94d9c7467d804
Sean Whitbeck <sean@liftoff.io>
2017-03-14 15:36:30 -0700

    [lein] Bump cider version

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0"]
+{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
                   ;; https://github.com/xsc/lein-ancient
                   [lein-ancient "0.6.10"]
