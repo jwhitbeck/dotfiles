7005464332b34017bcffb4b2fd0b982d14087b60
John Whitbeck <john@whitbeck.net>
2017-10-18 09:36:47 +0200

    [emacs] Provide dummy file for customizations

--- emacs/init.el
+++ emacs/init.el
@@ -10,6 +10,10 @@
 ;; This must come before configurations of installed packages. Emacs will insert this line if not present.
 (package-initialize)
 
+;;; Trick emacs into writing customizations to a dummy file so that they don't get written to this file.
+(custom-set-variables
+ '(custom-file (expand-file-name "custom.el" user-emacs-directory)))
+
 (require 'my-performance)
 (require 'my-locale)
 (require 'my-ui)
