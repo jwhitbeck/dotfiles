913e627b2e78029f82332bd2da0544a094c9d01f
John Whitbeck <john@whitbeck.net>
2017-10-20 10:02:36 +0200

    [emacs] Fix packages macro

--- emacs/me/lisp/my-package.el
+++ emacs/me/lisp/my-package.el
@@ -9,14 +9,16 @@
                                            ("melpa" . "https://melpa.org/packages/")
                                            ("melpa-stable" . "https://stable.melpa.org/packages/"))))
 
-;;; Refresh the package info if it has never been run before.
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(defvar my-packages-refreshed? nil
+  "t if we have refreshed the package info in this emacs session")
 
 (defmacro my-use-packages (&rest packages)
   "Ensures the provided PACKAGES are available locally and downloads them if necessary."
   `(dolist (pkg (quote (,@packages)))
      (when (not (package-installed-p pkg))
+       (when (not my-packages-refreshed?)
+         (package-refresh-contents)
+         (setq my-packages-refreshed? t))
        (package-install pkg))))
 
 ;;; Ensure that we use the latest org-mode with contribs. This needs to be installed before all other
