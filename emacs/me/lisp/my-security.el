40cae37cab27feaa1c35c424057b7878448f15da
John Whitbeck <john@whitbeck.net>
2018-07-20 15:31:47 +0200

    [emacs] Update TLS config

--- emacs/me/lisp/my-security.el
+++ emacs/me/lisp/my-security.el
@@ -7,6 +7,17 @@
 (require 'gnutls)
 (custom-set-variables
  '(gnutls-verify-error t)
+ '(gnutls-min-prime-bits 2048)
  '(gnutls-algorithm-priority "PFS"))
+
+;;; Even with these settings, emacs isn't very good at TLS security.
+;;; See https://news.ycombinator.com/item?id=17567347 for a more detailed discussion.
+;;;
+;;; I can test emacs TLS againt badssl urls using the following snippet.
+;; (mapcar (lambda (host)
+;;           (ignore-errors (url-retrieve-synchronously host)))
+;;         '("https://revoked.badssl.com/"
+;;           "https://pinning-test.badssl.com/"
+;;           "https://invalid-expected-sct.badssl.com/"))
 
 (provide 'my-security)
