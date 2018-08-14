c700cd0568ee6228878f59c4ab54b643ce40c701
sean <sean@sean-xps2>
2018-08-14 11:21:23 +0200

    [lein] Upgrade cider

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.17.0"]
                   ;; A Leiningen plugin to check your project for outdated dependencies and plugins.
                   ;; https://github.com/xsc/lein-ancient
                   [lein-ancient "0.6.10"]
