3831a564ccc5be313d8d82ce9acc00a8f0e94edf
John Whitbeck <john@whitbeck.net>
2019-02-25 16:21:14 +0100

    [emacs] Remove clj-refactor
    Increasingly incompatible with recent cider versions

--- emacs/me/lisp/my-clojure.el
+++ emacs/me/lisp/my-clojure.el
@@ -4,7 +4,7 @@
 
 (require 'my-package)
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-(my-use-packages auto-indent-mode cider clojure-mode clj-refactor kibit-helper)
+(my-use-packages auto-indent-mode cider clojure-mode kibit-helper)
 
 (add-hook 'clojure-mode-hook 'cider-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
@@ -39,9 +39,7 @@
    ;; Auto-focus the error buffer when it's displayed after evaluating some
    ;; clojure code. This makes it easy to type "q" to dismiss the window,
    ;; assuming you don't want this backtrace window hanging around.
-   '(cider-auto-select-error-buffer t)
-   ;; When using `cljr-clean-ns`, don't favor prefix notation in the ns form.
-   '(cljr-favor-prefix-notation nil))
+   '(cider-auto-select-error-buffer t))
 
   ;; Disable C-c SPC binding in clojure mode as it overrides the ace-window-mode
   ;; key binding
