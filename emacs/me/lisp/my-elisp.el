9ec080f1d672d62d32e9d2a6f50091b624273566
John Whitbeck <john@whitbeck.net>
2019-03-07 19:04:48 +0100

    [emacs] Add some string helper functions

--- emacs/me/lisp/my-elisp.el
+++ emacs/me/lisp/my-elisp.el
@@ -56,4 +56,26 @@ files on the load path."
   (message "Regenerating elisp tags")
   (my-generate-elisp-tags))
 
+;;; Simple functions for replacing certain chars in a string with another. Emacs
+;;; doesn't appear to have such functions built-in.
+(defun my-replace-string (s old new)
+  "Replace occurrences of 'old' string within string 's' with
+  'new' string."
+  (with-temp-buffer
+    (insert s)
+    (goto-char (point-min))
+    (while (search-forward old nil t)
+      (replace-match new))
+    (buffer-string)))
+
+(defun my-replace-string-re (s re new)
+  "Replace matches of regexp within string 's' with 'new'
+  string."
+  (with-temp-buffer
+    (insert s)
+    (goto-char (point-min))
+    (while (search-forward-regexp re nil t)
+      (replace-match new))
+    (buffer-string)))
+
 (provide 'my-elisp)
