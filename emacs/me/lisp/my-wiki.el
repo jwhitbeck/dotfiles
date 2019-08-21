953545284ac3d2524861d4c33eb2f7c8a9b3b89a
John Whitbeck <john@whitbeck.net>
2019-08-21 14:05:00 -0700

    [emacs] Refactor wikipedia helpers

--- emacs/me/lisp/my-wiki.el
+++ emacs/me/lisp/my-wiki.el
@@ -2,14 +2,25 @@
 
 ;;; Wikipedia quick access
 
-(defun my-browse-wikipedia (lang word &optional wiktionary?)
+(defun my-browse-wikimedia (site word prompt-language?)
+  (let ((lang (if prompt-language?
+                  (ido-completing-read "Language:" '("en" "fr"))
+                "en")))
+    (browse-url (format "https://%s.%s.org/wiki/Special:Search?search=%s"
+                        lang
+                        site
+                        word))))
+
+(defun my-loopkup-wikipedia (word &optional prompt-language?)
   (interactive
-   (list (ido-completing-read "Language:" '("en" "fr"))
-         (word-at-point)
+   (list (word-at-point)
          current-prefix-arg))
-  (browse-url (format "https://%s.%s.org/wiki/Special:Search?search=%s"
-                      lang
-                      (if wiktionary? "wiktionary" "wikipedia")
-                      word)))
+  (my-browse-wikimedia "wikipedia" word prompt-language?))
+
+(defun my-loopkup-wiktionary (word &optional prompt-language?)
+  (interactive
+   (list (word-at-point)
+         current-prefix-arg))
+  (my-browse-wikimedia "wiktionary" word prompt-language?))
 
 (provide 'my-wiki)
