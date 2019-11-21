ed0d328e37c1bac2228846525176be2d379499b7
John Whitbeck <john@whitbeck.net>
2019-11-20 16:48:02 -0800

    [emacs] Turn on whitespace mode in clojure

--- emacs/me/lisp/my-clojure-mode.el
+++ emacs/me/lisp/my-clojure-mode.el
@@ -8,6 +8,7 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'subword-mode)
 (add-hook 'clojure-mode-hook 'auto-indent-mode)
+(add-hook 'clojure-mode-hook 'my-whitespace-mode-default)
 (add-hook 'cider-mode-hook 'eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
