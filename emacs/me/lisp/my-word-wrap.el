1d30a6dc29d4f60197e301a4cebeb6409f0338bc
John Whitbeck <john@whitbeck.net>
2020-01-11 15:23:27 +0100

    [emacs] Improve word wrap toggle

--- emacs/me/lisp/my-word-wrap.el
+++ emacs/me/lisp/my-word-wrap.el
@@ -3,21 +3,30 @@
 ;;;; Cycle between word-wrapping settings
 (require 'visual-fill-column)
 
+(defvar-local my-word-wrap-state 'no-wrap
+  "One of no-wrap, wrap-window, or wrap-fill-column.")
+
 ;;;###autoload
-(defun my-word-wrap (&optional arg)
-  (interactive "p")
-  (cl-case arg
-    ;; long lines
-    (1 (progn (toggle-truncate-lines 1)
-	      (toggle-word-wrap 0)
-	      (visual-fill-column-mode 0)))
-    ;; wrap words at window boundary
-    (4 (progn (toggle-truncate-lines 0)
-	      (toggle-word-wrap 1)
-	      (visual-fill-column-mode 0)))
-    ;; wrap words at fill-column
-    (16 (progn (toggle-truncate-lines 0)
-	       (toggle-word-wrap 1)
-	       (visual-fill-column-mode 1)))))
+(defun my-word-wrap ()
+  (interactive)
+  (cl-case my-word-wrap-state
+    ;; Switch to wrap-window
+    ('no-wrap (progn (toggle-truncate-lines 0)
+                     (toggle-word-wrap 1)
+                     (visual-fill-column-mode 0)
+                     (setq my-word-wrap-state 'wrap-window)
+                     (message "Wrap text at window edge.")))
+    ;; Switch to wrap-fill-column
+    ('wrap-window (progn (toggle-truncate-lines 0)
+                         (toggle-word-wrap 1)
+                         (visual-fill-column-mode 1)
+                         (setq my-word-wrap-state 'wrap-fill-column)
+                         (message "Wrap text at fill column.")))
+    ;; Switch to long lines
+    ('wrap-fill-column (progn (toggle-truncate-lines 1)
+                              (toggle-word-wrap 0)
+                              (visual-fill-column-mode 0)
+                              (setq my-word-wrap-state 'no-wrap)
+                              (message "Text wrapping disabled.")))))
 
 (provide 'my-word-wrap)
