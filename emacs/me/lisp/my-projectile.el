18b2a997c25763943aff59822b35e37ead11eb4a
John Whitbeck <john@whitbeck.net>
2020-01-15 13:49:48 +0100

    [emacs] Better projectile-switch-project-action

--- emacs/me/lisp/my-projectile.el
+++ emacs/me/lisp/my-projectile.el
@@ -7,7 +7,10 @@
  ;; Use git-grep rather than grep for git projects
  projectile-use-git-grep t
  ;; Use ivy for completions.
- projectile-completion-system 'ivy)
+ projectile-completion-system 'ivy
+ ;; When running C-p p to switch projects, prompt for the next projectile
+ ;; command instead of running projectile-find-file.
+ projectile-switch-project-action 'projectile-commander)
 
 ;;; Drop an empty .projectile_root file in a dir for projectile to consider it a
 ;;; project root
