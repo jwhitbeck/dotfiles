726ce940b7a7cb12155f20c67ae19239bd921cf7
John Whitbeck <john@whitbeck.net>
2020-02-18 22:28:59 +0100

    [emacs] Disable fuzzy matching in ivy

--- emacs/me/lisp/my-ivy.el
+++ emacs/me/lisp/my-ivy.el
@@ -15,14 +15,6 @@
 (my-mode-line-hide-minor-mode 'ivy-mode)
 (my-mode-line-hide-minor-mode 'counsel-mode)
 
-;;; Default to `ivy' matching, but use `fuzzy' matching (via flx) for some
-;;; callers (i.e., the value in `this-command').
-(setq ivy-re-builders-alist
-      '((counsel-M-x . ivy--regex-fuzzy)
-        (counsel-describe-function . ivy--regex-fuzzy)
-        (counsel-describe-variable . ivy--regex-fuzzy)
-        (t . ivy--regex-plus)))
-
 (setq
  ;; add recent files and/or bookmarks to ‘ivy-switch-buffer’.
  ivy-use-virtual-buffers t
