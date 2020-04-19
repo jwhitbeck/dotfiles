788ab7b6da1e668696ea5e200bddce45f6c2e2c3
John Whitbeck <john@whitbeck.net>
2020-04-19 23:41:03 +0200

    [emacs] Hide some powerline elements

--- emacs/me/lisp/my-mode-line.el
+++ emacs/me/lisp/my-mode-line.el
@@ -6,6 +6,10 @@
 (require 'powerline)
 (powerline-default-theme)
 
+;;; Powerline customizations
+(setq powerline-display-buffer-size nil
+      powerline-display-hud nil)
+
 (defun my-mode-line-hide-minor-mode (mode)
   "Hide the minor mode from the mode line. Useful to hide
   globally enabled minor modes."
