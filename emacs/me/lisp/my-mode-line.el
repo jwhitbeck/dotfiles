0f24b908aa030a5d3b20748e48b6afdb8225ab1d
John Whitbeck <john@whitbeck.net>
2020-04-26 22:28:39 +0200

    [emacs] Better org-mode faces in powerline

--- emacs/me/lisp/my-mode-line.el
+++ emacs/me/lisp/my-mode-line.el
@@ -10,6 +10,10 @@
 (setq powerline-display-buffer-size nil
       powerline-display-hud nil)
 
+;;; Clear these faces to use the default powerline faces instead
+(setq face-remapping-alist
+      '((org-mode-line-clock)))
+
 (defun my-mode-line-hide-minor-mode (mode)
   "Hide the minor mode from the mode line. Useful to hide
   globally enabled minor modes."
