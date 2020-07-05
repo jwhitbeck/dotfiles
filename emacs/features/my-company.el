3c0e77935ea568b34dde38a5be2bb900178cc37d
John Whitbeck <john@whitbeck.net>
2020-07-05 19:13:59 +0200

    [emacs] Company-mode tweaks

--- emacs/features/my-company.el
+++ emacs/features/my-company.el
@@ -3,7 +3,15 @@
 ;;;; Auto-completion using company
 (require 'company)
 
-(global-company-mode)
+;;; This is the setup recommended by the package authors.
+(add-hook 'after-init-hook 'global-company-mode)
+
+;;; Consistent key bindings with ivy
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-f") 'company-filter-candidates)
 
 ;;; Hide minor mode indicator
 (setq company-lighter nil)
