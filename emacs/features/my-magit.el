59c909b166363b8dfb14eec1bd7bc2c825413232
John Whitbeck <john@whitbeck.net>
2020-12-28 11:41:42 +0100

    [emacs] Remove old magit override

--- emacs/features/my-magit.el
+++ emacs/features/my-magit.el
@@ -5,7 +5,4 @@
 
 (setq magit-delete-by-moving-to-trash nil)
 
-;;; Don't let magit rebind C-x g, which I bind to vc-git-grep.
-(define-key magit-file-mode-map (kbd "C-x g") nil)
-
 (provide 'my-magit)
