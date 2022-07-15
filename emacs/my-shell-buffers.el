537a4c79018dd873222019a645e2dd06d3a77372
John Whitbeck <john@whitbeck.net>
2022-07-15 23:43:14 +0200

    [emacs] Improve shell buffers

--- emacs/my-shell-buffers.el
+++ emacs/my-shell-buffers.el
@@ -21,17 +21,35 @@
                         (mapcar #'buffer-name bufs)
                         nil t)))))
 
+(defun my-shell--gen-buffer-name ()
+  (let ((stem (if (not (eq 'shell-mode major-mode))
+                  "*shell*"
+                (let ((bufname (buffer-name)))
+                  (with-temp-buffer
+                    (insert bufname)
+                    (if (re-search-backward "<[[:digit:]]+>" nil t)
+                        (buffer-substring (point-min) (point))
+                      bufname))))))
+    (generate-new-buffer-name stem)))
+
 ;;;###autoload
 (defun my-shell (&optional arg)
   "Like `shell' but always opens the *shell* buffer in the
-current window. With a C-u prefix, opens a new shell. With C-u
-C-u prefix prompts for the new buffer's name. If the buffer
-doesn't exist and no prefix arg is given, create it in the home
-directory."
+current window.
+
+With a C-u prefix, starts a shell in a new buffer. If the current
+buffer is a shell buffer, the new one will be named after
+it (e.g., *foo*<2> if the current buffer is called
+*foo*). Otherwise, use *shell*<N>.
+
+With C-u C-u prefix, always prompts for the new buffer's name.
+
+If the buffer doesn't exist and no prefix arg is given, set the
+directory to the the home directory."
   (interactive "P")
   (let* ((buf-name (cond ((null arg) "*shell*")
-                        ((equal arg '(4)) (generate-new-buffer-name "*shell*"))
-                        ((equal arg '(16)) (read-buffer "Name: "))
+                         ((equal arg '(4)) (my-shell--gen-buffer-name))
+                         ((equal arg '(16)) (read-string "Buffer name: " (my-shell--gen-buffer-name)))
                         (t (error "Invalid arg %s" arg))))
          (buf (get-buffer buf-name)))
     (unless buf
